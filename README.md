7c512b038d26ea6543714ff917577e817ee8da2e
thanh quach <thanhb.quach@gmail.com>
2022-09-29 21:33:44 -0400

    get IPFS Metadata

---
+++ README.md
@@ -0,0 +1,3 @@
+# BasicNFt.sol Contract
+We need to upload a file to IPFS and get the link (the pointer) which will be included in our
+tokenURI(a jason file with Metadata)
--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -2,3 +2,23 @@
 
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string public constant TOKEN_URI = "https://ipfs.io/ipfs/Qmaj8PbDfTc9JzVLCF4XvXE5qkqrVGyBoUjqs1QXJCnB8D?filename=image.json"
+
+    constructor() ERC721("ThanhTheMan", "THANH") {
+        s_tokenCounter = 0
+    }
+
+    function mintNft() public returns(uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
+        return s_tokenCounter;
+    }
+
+    function getTokenCounter() public view returns(uint256) {
+        return s_tokenCounter;
+    }
+}
